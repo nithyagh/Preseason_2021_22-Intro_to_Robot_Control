a211110689e1f7d99e8f523ce6d138815e869be2
NithyaS <nithyas729@gmail.com>
2021-10-02 14:11:33 -0700

    Updated TimedAuto

--- src/main/java/frc/robot/commands/TimedAuto.java
+++ src/main/java/frc/robot/commands/TimedAuto.java
@@ -45,6 +45,6 @@ public class TimedAuto extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return (timer.get() > 5);
   }
 }
